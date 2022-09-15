f618da48871fd98679cb6f69c5b4e95ddea7bc59
Aleksi Tuukkanen <caifudigital@gmail.com>
2022-09-15 11:31:44 +0300

    LINQ works

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Exercise8;
+
 Console.WriteLine("Hello, World!");
 
 
 Student objectStudent = new Student("Teppo", "Testi");
 Console.WriteLine(objectStudent.Fname + " " + objectStudent.Lname);
+
+List<Student> studentsCollection = new List<Student>();
+studentsCollection.Add(new Student { Fname = "Ann", Lname = "Simpson"});
+studentsCollection.Add(new Student { Fname = "Jim", Lname = "Smith"});
+
+studentsCollection.Add(new Student("Bob", "Smith"));
+
+foreach(Student stu in studentsCollection){
+    Console.WriteLine(stu.Fname + " " + stu.Lname); 
+}
+
+var chosenStudents = from chosenStu in studentsCollection
+where chosenStu.Lname.Equals("Smith")
+select chosenStu;
+
+Console.WriteLine("Chosen Students");
+foreach(Student stu in chosenStudents){
+    Console.WriteLine(stu.Fname + " " + stu.Lname); 
+}
